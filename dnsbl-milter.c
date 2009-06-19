05cdb1cac20a5ec088aa18282bcd8b217d567824
Haw Loeung <hloeung@users.sourceforge.net>
2009-06-19 18:03:34 +1000

    - added GPL license to dnsbl-milter.c

--- dnsbl-milter.c
+++ dnsbl-milter.c
@@ -1,10 +1,23 @@
 /**
  * $Id$
  *
- * dnsbl-milter.c  by Haw Loeung <hloeung@users.sourceforge.net>
+ * dnsbl-milter.c - lightweight sendmail DNSBL & DNSWL milter
+ * <http://dnsbl-milter.sourceforge.net/>
  *
- * Sendmail milter used to consult a list of DNS blacklists & whitelists to see
- * if messages from particular hosts should be accepted
+ * Copyright 2007, 2008, 2009 Haw Loeung <hloeung@users.sourceforge.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 

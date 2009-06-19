1083ddc929b3374da1d406fdd3e5fd0600301e6c
Haw Loeung <hloeung@users.sourceforge.net>
2009-06-19 18:14:33 +1000

    - fixed bug #2805155. ctime_r() has been marked as obsolete in   POSIX.1-2008

--- dnsbl-milter.c
+++ dnsbl-milter.c
@@ -364,8 +364,9 @@ $Id$\n");
 
 static void mlog(const int priority, const char *fmt, ...)
 {
-	char tbuf[28];
+	char tbuf[15];
 	time_t t;
+	struct tm tm;
 	va_list ap;
 
 	va_start(ap, fmt);
@@ -376,7 +377,8 @@ static void mlog(const int priority, const char *fmt, ...)
 
 	else {
 		t = time(NULL);
-		fprintf(stderr, "%.15s ", ctime_r(&t, tbuf) + 4);
+		strftime(tbuf, sizeof(tbuf), "%b %e %T", localtime_r(&t, &tm));
+		fprintf(stderr, "%.15s ", tbuf);
 		vfprintf(stderr, fmt, ap);
 		fprintf(stderr, "\n");
 		fflush(stderr);
